fb97f1022056c697ce0589d18e08bd7ada1a45d1
Rohith <rohithneela369@gmail.com>
2026-03-28 12:35:22 +0530

    Add project files.

---
+++ EmployeeManagementSystem/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Day 1 of project is Done!");
+            Console.WriteLine("Project has started!!!!");
+        }
+    }
+}
