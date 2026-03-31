610c49675b96b982f632f78c87a3388affe277e4
Rohith <rohithneela369@gmail.com>
2026-03-31 22:39:05 +0530

    simulate async employee loading

--- EmployeeManagementSystem/Program.cs
+++ EmployeeManagementSystem/Program.cs
@@ -3,19 +3,38 @@ namespace EmployeeManagementSystem
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Employee emp = new Employee();
-            emp.SetName("Rohith");
-            emp.SetSalary(30000);
-            emp.DisplayInfo();
-            Manager mgr = new Manager();
-            mgr.SetName("Suresh");
-            mgr.SetSalary(100000);
-            mgr.SetTeamSize(5);
-            mgr.DisplayInfo();
 
-            Console.WriteLine("Day 2 of project Done!");
+            await LoadEmployeesAsync();
+            await SaveEmployeesAsync();
+            int count = await GetEmployeeCountAsync();
+            Console.WriteLine("$Total Employees: {count}");
+            Console.WriteLine("\n Day 5 Completed!!!");
+
+
         }
-    }
+
+
+        static async Task LoadEmployeesAsync()
+        {
+            Console.WriteLine("Loading employee from database...");
+            await Task.Delay(2000);
+            Console.WriteLine("Employee loaded successfully!");
+        }
+
+        static async Task SaveEmployeesAsync()
+        {
+            Console.WriteLine("Saving employees from data base....");
+            await Task.Delay(1000);
+            Console.WriteLine("Employees saved successfully!");
+        }
+
+        static async Task<int> GetEmployeeCountAsync()
+        {
+            Console.WriteLine("Fetching employeee count....");
+            await Task.Delay(1500);
+            return 5;
+        }
+      }
 }
