e91b85add7305c2a4cada8d683c1c4a94e99eb1b
Rohith <rohithneela369@gmail.com>
2026-04-03 01:10:34 +0530

    chore: week1 revision complete

--- EmployeeManagementSystem/Program.cs
+++ EmployeeManagementSystem/Program.cs
@@ -3,51 +3,12 @@ using System.Threading.Tasks;
 
 namespace EmployeeManagementSystem
 {
-    internal class Program
+    internal class Program:Revision
     {
         static async Task Main(string[] args)
         {
-            // Day 2 - OOP
-            Employee emp = new Employee();
-            emp.SetName("Rohith");
-            emp.SetSalary(30000);
-            emp.DisplayInfo();
-
-            Manager mgr = new Manager();
-            mgr.SetName("Suresh");
-            mgr.SetSalary(100000);
-            mgr.SetTeamSize(5);
-            mgr.DisplayInfo();
-
-            // Day 5 - Async
-            await LoadEmployeesAsync();
-            await SaveEmployeesAsync();
-
-            int count = await GetEmployeeCountAsync();
-            Console.WriteLine($"Total Employees: {count}");
-
-            Console.WriteLine("\n Day 5 Completed!!!");
+            await RevisionProgram.RunRevision();
         }
 
-        static async Task LoadEmployeesAsync()
-        {
-            Console.WriteLine("Loading employee from database...");
-            await Task.Delay(2000);
-            Console.WriteLine("Employee loaded successfully!");
-        }
-
-        static async Task SaveEmployeesAsync()
-        {
-            Console.WriteLine("Saving employees...");
-            await Task.Delay(1000);
-            Console.WriteLine("Employees saved successfully!");
-        }
-
-        static async Task<int> GetEmployeeCountAsync()
-        {
-            Console.WriteLine("Fetching employee count...");
-            await Task.Delay(1500);
-            return 5;
-        }
     }
 }
---
+++ EmployeeManagementSystem/Revision.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeManagementSystem
+{
+    public class Revision
+    {
+        public interface IEmployee
+        {
+            void DisplayInfo();
+            double CalculateSalary();
+        }
+        public abstract class BaseEmployee
+        {
+            private string _name;
+            public void SetName(string name)
+            {
+                if(string.IsNullOrEmpty(name))
+                {
+                    Console.WriteLine("Name Cannot be Empty");
+                    return;
+                }
+                _name = name;
+            }
+            public string GetName()
+            {
+                return _name;
+            }
+
+            public abstract void DisplayInfo();
+
+        }
+        public class Employee : BaseEmployee, IEmployee
+        {
+          private double _salary;
+          public void SetSalary(double salary)
+            {
+                if (salary < 0)
+                {
+                    Console.WriteLine("Salary cannot be negitive");
+                    return;
+                }
+                _salary = salary;
+            }
+            public double GetSalary() => _salary;
+            public double CalculateSalary()
+            {
+                return _salary;
+            }
+
+            public override void DisplayInfo()
+            {
+                Console.WriteLine($"Employee:{GetName()} and Salary:{GetSalary()} and Calculate Salary {CalculateSalary()}");
+            }
+
+        }
+        public class Manager : BaseEmployee, IEmployee
+        {
+            private double _salary;
+            private int _teamSize;
+            public void SetSalary(double salary)
+            {
+                if (salary < 0)
+                {
+                    Console.WriteLine("Salary cannot be negitive");
+                    return;
+                }
+                _salary = salary;
+            
+            }
+            public double GetSalary() => _salary;
+            public void SetTeamSize(int size)
+            {
+                _teamSize = size;
+            }
+            public double CalculateSalary()
+            {
+                return  _salary + (_salary * 0.20);
+
+            }
+            public override void DisplayInfo()
+            {
+                Console.WriteLine($"Manager:{GetName()} Salary:{GetSalary()}  CalculatedSalary:{CalculateSalary()}");
+            }
+
+        }
+        public class RevisionProgram
+        {
+            public static async Task RunRevision()
+            {
+                List<Employee> employees = new List<Employee>();
+                Employee e1 = new Employee();
+                e1.SetName("Rohith");
+                e1.SetSalary(70000);
+                employees.Add(e1);
+
+                Employee e2 = new Employee();
+                e2.SetName("Ramu");
+                e2.SetSalary(60000);
+                employees.Add(e2);
+
+                Employee e3 = new Employee();
+                e3.SetName("ASHOK");
+                e3.SetSalary(45000);
+                employees.Add(e3);
+
+                Console.WriteLine("--------------SALARY ABOVE 4OK----------");
+                var filtered = employees.Where(e => e.GetSalary() > 40000);
+                foreach (var e in filtered)
+                {
+                    e.DisplayInfo();
+                }
+                Console.WriteLine("\n-------------Sorted by Salary----------");
+                var sorted = employees.OrderByDescending(E => E.GetSalary());
+                foreach (var E in sorted)
+                {
+                    E.DisplayInfo();
+
+                }
+
+                await LoadDataAsync();
+                Console.WriteLine("\nWeek 1 Revision Complete ");
+
+
+            }
+            static async Task LoadDataAsync()
+            {
+                Console.WriteLine("Loading data from database...");
+                await Task.Delay(2000);
+                Console.WriteLine("Data loaded successfully!");
+            }
+        }
+
+
+    }
+    
+}
